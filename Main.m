8d58bdc87d46c932675dc98fd908773f8ed008b1
Josh Bevan <userjjb@gmail.com>
2014-03-27 04:32:05 -0400

    Generalization of code
    Code generalized/"vectorized"

--- Main.m
+++ Main.m
@@ -6,46 +6,46 @@ clear all
 %%Let vh be the finite vector space of linear polynomials
 %%Let uh be the approximate numerical solution consisting of a linear 
 %combination of basis functions (\thetaN for the Nth basis) in vh with
-%scalar coefficients aN (i.e. \sum aN \thetaN)
+%scalar coefficients BasisWeightN (i.e. \sum BasisWeightN \thetaN)
 %%Let \phiN be the Nth test function in the same vector space as the basis
 %functions (vh)
 
 %%Discretize the domain into K elements with K+1 nodes
 tau=2*pi();
 
-K=10;
-x=0:1/K:1;
-deltax = diff(x);
+K=20;
+xNode=0:1/K:1;
+deltax = diff(xNode);
 %All the nodes, note their are repeats since nodes have a coincident
 %brother from an adjacent element, except at the boundaries
-xh = sort([x,x(2:end-1)]);
+xh = sort([xNode,xNode(2:end-1)]);
 
-xu = 0:1/100:1;
+xf = 0:1/100:1;
 u= sin(tau.*xf);
 
 %%According to Cockburn,Shu 2001 Eqn 2.2 let uh(.,0) be computed by
 %\int uh \phi = \int u0 \phi for each element (xj-1/2 < x < xj=1/2)
-%%We can explicitly define a formula for cN = \int u0 \phiN the value of
+%%We can explicitly define a formula for ExactRHSN = \int u0 \phiN the value of
 %the RHS
-%%NOTE: There is an extra factor of 1/deltax here that was moved from the LHS
-c1= (tau*deltax.*cos(tau.*x(1:end-1))+sin(tau.*x(1:end-1))-sin(tau.*x(2:end)))./(4*pi()^2.*(deltax).^2);
-c2= -(tau*deltax.*cos(tau.*x(2:end))+sin(tau.*x(1:end-1))-sin(tau.*x(2:end)))./(4*pi()^2.*(deltax).^2);
+ExactRHS(1,:)= (tau*deltax.*cos(tau.*xNode(1:end-1))+sin(tau.*xNode(1:end-1))-sin(tau.*xNode(2:end)))./(4*pi()^2.*(deltax));
+ExactRHS(2,:)= -(tau*deltax.*cos(tau.*xNode(2:end))+sin(tau.*xNode(1:end-1))-sin(tau.*xNode(2:end)))./(4*pi()^2.*(deltax));
 
-%%The LHS requires \int uh \phi which is \int \sum(aN \thetaN) \phi
-%Since both \phi and \theta belong to vh this becomes:
-%\sum aN \int \theta_N \theta_M
-%where possibly M=N.
-%%Here N<=2 so we only need two aN for the two possibilities for the 
-%integral.
-%%We find that the LHS integral is either h/3 for M=N or h/6 for M!=N, we
-%%then solve algebraically for aN
-a1 = 4*c1 - 2*c2;
-a2 = 4*c2 - 2*c1;
+%%The LHS requires calculation of \int uh \phi
+%which is \int \sum(aN \thetaN) \phi
+
+%Matrix of LHS integrals of form \int \thetaI \phiJ, where \thetaI=\phiI
+LHSIntegrals = [1/3 1/6;
+                1/6 1/3];
+%%Calculate basis weights for approximate solution
+%LHSIntegrals * BasisWeights = ExactRHS / deltax
+BasisWeights = LHSIntegrals\(ExactRHS./repmat(deltax,size(ExactRHS,1),1));
 
 %%Mapping uh0 values to the appropriate element nodes
-uh(1:2:length(xh)) = a1;
-uh(2:2:length(xh)) = a2;
+uh(1:2:length(xh)) = BasisWeights(1,:);
+uh(2:2:length(xh)) = BasisWeights(2,:);
 
-plot(xu,u)
+plot(xf,u,'b')
 hold on
-plot(xh,uh)
+plot(xh,uh,'r')
+
+BasisWeights_dt = (1./deltax).*(1)
