5c2b60688073822afc1c1de41a9e9e6bcddfbbeb
Josh Bevan <userjjb@gmail.com>
2014-05-26 22:26:12 -0400

    RK4 implemented

--- SC1DLeg.m
+++ SC1DLeg.m
@@ -1,11 +1,13 @@
-%--Josh Bevan 2014
-%--1D Scalar Conservation Eqn solution using Discontinuous Galerkin
+%Josh Bevan 2014
+%1D Scalar Conservation Eqn solution using Discontinuous Galerkin
+%Arbitrary order spatial discretization using a Legendre polynomial basis
+%and RK4 explicit time discretization
 close all
 clear all
-
 tau=2*pi();
-N=3;
-K=32;
+
+N=3; %Order of spatial discretization
+K=32;%Number of elements
 
 %--Here we attempt to solve the 1D scalar conservation eqn of the form:
 %\frac{\partial u)(\partial t} + \frac{\partial f(u))(\partial x} = 0
@@ -14,9 +16,15 @@ K=32;
 %We will use periodic boundary conditions to examine numerical dissipation
 %effects
 
+%Discretize the domain into K elements with K+1 nodes, we use a constant
+%spacing, but it could be arbitrary
+xNode=0:1/K:1;
+%All the element boundaries, note there are repeats since nodes have a coincident
+%brother from an adjacent element, except at the domain boundaries
+elemBC = reshape(sort([xNode,xNode(2:end-1)]),2,K)';
+
 %--Let u(x,t) be the exact solution on the domain 0<=x<=1
 %--Let u(.,0) = u0 = sin(2pi * x)
-
 %--Let Vh be the finite vector space of the shifted Legendre polynomials
 %up to order N.
 %--Let v be the approximate numerical solution consisting of a linear 
@@ -25,14 +33,6 @@ K=32;
 %--Let \phi_n be the nth test function in the same vector space as the basis
 %functions (Vh)   
 
-%--Discretize the domain into K elements with K+1 nodes, we use a constant
-%spacing, but it could be arbitrary
-xNode=0:1/K:1;
-deltax = diff(xNode);
-%All the element boundaries, note there are repeats since nodes have a coincident
-%brother from an adjacent element, except at the domain boundaries
-elemBC = reshape(sort([xNode,xNode(2:end-1)]),2,K)';
-
 %--According to Cockburn,Shu 2001 Eqn 2.2 let uh(.,0) be computed by
 %\int v \phi = \int u0 \phi for each element (xk-1/2 < x < xk+1/2)
 %--We will numerically compute the RHS integral using Gauss-Legendre
@@ -40,7 +40,6 @@ elemBC = reshape(sort([xNode,xNode(2:end-1)]),2,K)';
 
 %Precalculate quadrature nodes and weights 
 [Qx,Qw]=gauss(N+1);
-
 %Precompute Legendre values for fixed order quadrature points
 L = zeros(N+1);
 for m=0:N
@@ -65,8 +64,8 @@ for k=1:K
     end
 end
 
-%--Now that we have u0 we can begin explicit time stepping (forward Euler)
-%with the semi-discrete form of the PDE.
+%--Now that we have u0 we can begin explicit time stepping with the 
+%semi-discrete form of the PDE.
 %--Because we have a linear flux function all the classic monotone flux
 %schemes reduce to the simple upwind flux i.e. g(v-(x),g(v+(x))=v-(x)
 
@@ -74,53 +73,50 @@ end
 %the upwind flux and the RHS integral in the original PDE. The RHS integral
 %portion is a strictly lower triangular matrix
 SelfStencil = 2*(toeplitz(mod(0:N,2),0:N<0))-ones(N+1);
-
 %References to the bases in the upwind element, consists of the other part
 %of the upwind flux
 UpwindStencil = ones(N+1); UpwindStencil(2:2:N+1,:)=-1;
-
 %LHSIntegral is a purely diagonal matrix that depends on element size and
 %the order of each basis
-LHSIntegral = reshape([1:2:2*N+1]'*(1./deltax),K*(N+1),1);
+LHSIntegral = reshape([1:2:2*N+1]'*(1./diff(xNode)),K*(N+1),1);
 
 %Build a unit stencil to pull the global diagonal pattern from
-C=SelfStencil; C(N+2:2*N+2,1:N+1)=UpwindStencil;
+A=SelfStencil; A(N+2:2*N+2,1:N+1)=UpwindStencil;
 DiagStencil = zeros(N+1,3*N+2);
-for d=-(2*N+1):N
-    DiagStencil(1:length(diag(C,d)),d+2*N+2) = diag(C,d);
+for i=-(2*N+1):N
+    DiagStencil(1:length(diag(A,i)),i+2*N+2) = diag(A,i);
 end
-
 %Flip superdiagonal since spdiags truncates the start of any oversized
 %superdiagonals
 DiagStencil(:,2*N+2:3*N+2) = flipud(DiagStencil(:,2*N+2:3*N+2));
 
 %Build sparse reference matrix by repeating unit stencil for each element
 A = spdiags(repmat(DiagStencil,K,1),-(2*N+1):N,K*(N+1),K*(N+1));
+%Periodic BCs mean the upwind element for k=1 is k=K 
 A(1:N+1,K*(N+1)-N:K*(N+1))=UpwindStencil;
-spy(A)
 
 %Calculate new da/dt
-%BasisWeights_dt=LHSIntegral.*(A*reshape(BasisWeights',K*(N+1),1));
 BasisWeights = reshape(BasisWeights',K*(N+1),1);
     
 %--Discretize in time and plot
-deltaT= .0001;
-saveT = 0.05;
-endT = 10;
-
-nsaveT = floor(saveT/deltaT);
-nT = floor(endT/deltaT);
-%saved = zeros(N+1,K,(nT/nsaveT)+1);
+deltaT  = 0.001;
+saveT   = 0.01; %How often do we save the current state for plotting?
+endT    = 10;
+nsaveT  = floor(saveT/deltaT);
+nT      = floor(endT/deltaT);
+saved   = zeros(K*(N+1),(nT/nsaveT)+1);
+saved(:,1)=BasisWeights;
 i=1;
-
-saved(:,:,1)=reshape(BasisWeights,N+1,K);
 for t= 1:1:nT
     if t/nsaveT==floor(t/nsaveT)    
         i= i+1;
-        saved(:,:,i)=reshape(BasisWeights,N+1,K);
+        saved(:,i)=BasisWeights;
     end
-    BasisWeights_dt=LHSIntegral.*(A*BasisWeights);
-    BasisWeights = BasisWeights+(BasisWeights_dt.*deltaT);
+    k1 = LHSIntegral.*(A*BasisWeights);
+    k2 = LHSIntegral.*(A* (BasisWeights + deltaT*k1 /2) );
+    k3 = LHSIntegral.*(A* (BasisWeights + deltaT*k2 /2) );
+    k4 = LHSIntegral.*(A* (BasisWeights + deltaT*k3) );
+    BasisWeights = BasisWeights +(deltaT/6)*(k1 + 2*k2 + 2*k3 + k4);
 end
 
 Lobatto = zeros(N+3,N+1);
@@ -130,10 +126,10 @@ for m=0:N
 end
 j=0;
 for i=1:length(saved)
-    plot([elemBC(:,1) map elemBC(:,2)]',(Lobatto*saved(:,:,i)),'o-')
+    plot([elemBC(:,1) map elemBC(:,2)]',(Lobatto*reshape(saved(:,i),N+1,K)))
     axis([0 1 -1.5 1.5])
     text(1.02,-0.2,'RMS')
-    text(1.02,-.3,num2str(rms(reshape(L*saved(:,:,i),K*(N+1),1))));
+    text(1.02,-.3,num2str(rms(reshape(L*reshape(saved(:,i),N+1,K),K*(N+1),1))));
     text(1.02,1.1,'Time')
     text(1.02,1,num2str((i-1)*saveT));
     pause(.01)
