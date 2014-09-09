d0bb7e1fd1a9888ab26b101bbb87e5b30f57b0b9
Josh Bevan <userjjb@gmail.com>
2014-09-09 01:22:18 -0400

    LGL quadrature calc and mass matrix inspector

--- Utils/LagrangeMass.m
+++ Utils/LagrangeMass.m
@@ -1,28 +1,29 @@
 function [M,M_] = LagrangeMass(m,Type)
+switch Type
+    case 'GL'
+        [n,~] = GL_Quad(m);
+    case 'LGL'
+        [n,~,~] = LGL_Quad(m);
+    otherwise
+        return
+end
+
 for a=1:m
     for b=1:m
-      M(a,b)=InnerProd(m*Type,a,b);
+      M(a,b)=InnerProd(n,a,b);
     end
 end
 M_=inv(M);
 end
 
-function q = InnerProd(m,a,b)%,c)
-if m>0
-    [n,w] = gauss(m);
-elseif m<0
-    n=[-1,-sqrt(1/5),sqrt(1/5),1];
-    m=abs(m);
-end
-
+function q = InnerProd(n,a,b)
+m=numel(n);
 
 aa=[1:a-1,a+1:m];
 bb=[1:b-1,b+1:m];
 
-fun = @(x) ( (x-n(aa(1))).*(x-n(aa(2))).*(x-n(aa(3))) )...
-    ./( (n(a)-n(aa(1))).*(n(a)-n(aa(2))).*(n(a)-n(aa(3))) )...
-    .*( (x-n(bb(1))).*(x-n(bb(2))).*(x-n(bb(3))) )...
-    ./( (n(b)-n(bb(1))).*(n(b)-n(bb(2))).*(n(b)-n(bb(3))) );%...
-    %.*( 1./(x-n(c)) );
+fun = @(x) prod(bsxfun(@rdivide, bsxfun(@minus,x,n(aa)) ,n(a)-n(aa)))...
+    .*prod(bsxfun(@rdivide, bsxfun(@minus,x,n(bb)) ,n(b)-n(bb)));
+
 q= integral(fun,-1,1);
 end
