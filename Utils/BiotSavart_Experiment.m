0f580a2806809227dc998f3c9fce5c6c465ec9a4
Josh Bevan <userjjb@gmail.com>
2014-10-27 18:25:36 -0400

    External vorticity correction
    Added test code to examine cases where vorticity is not spatially
    bounded in the self domain and must receive external corrections from
    neighboring domains to remove the surface singularity at the boundary of
    the self domain.

--- Utils/BiotSavart_Experiment.m
+++ Utils/BiotSavart_Experiment.m
@@ -3,21 +3,22 @@ clear all
 clc
 
 %Local vorticity functions
-fun = @(x) 1-x.^2/2+x.^4/24-x.^6/720;%+x.^8/40320;
-%fun = @(x) 1-(pi^2*x.^2)/2+(pi^4*x.^4)/24-(pi^6*x.^6)/720;  %Taylor trig
-%fun = @(x) max(0, exp(-x.^2/.1));                          %Gaussian curve
-%fun = @(x) max(0,cos(pi * x)+1 - 2*heaviside(abs(x)-1));   %Cleaved trig
+%fun = @(x) 1-x.^2/2+x.^4/24-x.^6/720+x.^8/40320-x.^10/3628800; %Taylor trig
+%fun = @(x) 1-(pi^2*x.^2)/2+(pi^4*x.^4)/24-(pi^6*x.^6)/720;     %Taylor trig
+%fun = @(x) max(0, exp(-x.^2/.1));                              %Gaussian curve
+%fun = @(x) max(0,cos(x)) - heaviside(abs(x)-1));       %Cleaved trig
 A=-1; %Left boundary
 B=1; %Right Boundary
 
 xx=A:(B-A)/200:B;
+hold on
 plot(xx,10*fun(xx),'k') %Exaggerate vorticity function for display purposes
 
 %Domain decomposition method
 N=3;
 [Qx,Qw]=GLquad(N);
 
-K=100;
+K=1000;
 Nodes = A:(B-A)/K:B;
 El = Nodes(1:end-1);
 Er = Nodes(2:end);
@@ -35,25 +36,38 @@ for i=resolve
     Decomp(saved) = (Qw*[ fun(map(:,NOi)) .* (Centroids(i)-map(:,NOi))./abs(Centroids(i)-map(:,NOi)).^3 ])*deltax(NOi)'/2;
     saved=saved+1;
 end
-hold on
 plot(Centroids(resolve),Decomp,'b')
 close(h)
 
-%Global quadrature with singularity omission
-h = waitbar(0,'Initializing GL points please wait...');
-N=500;
-[Qx,Qw]=GLquad(N);
-Qx = Qx*(B-A)/2+(B+A)/2;
-saved=1;
-resolve = 1:round(N/numel(Decomp)):N; %Evaluate at same frequency as Decomp for speed comparison purposes
-for i=resolve
-    waitbar(saved/numel(resolve),h,'Global Omit method...')
-    NOi = [1:i-1,i+1:N];
-    GlobOmit(saved) = Qw(NOi)*[ fun(Qx(NOi)) .* (Qx(i)-Qx(NOi))./abs(Qx(i)-Qx(NOi)).^3 ];
-    saved=saved+1;
-end
-GlobOmit = GlobOmit*(B-A)/2;
-plot(Qx(resolve),GlobOmit,'r')
+% %Correct for external vorticity for each of the resolved points
+% cut=1.048; %How far into external domain corrected quadratures extend
+% N=100;
+% [Qx,Qw]=GLquad(N);
+% QxR = Qx*(pi/cut-B)/2+(pi/cut+B)/2;
+% QxL = Qx*(A--pi/cut)/2+(A+-pi/cut)/2;
+% saved=1;
+% for i=Centroids(resolve)
+%     ExtR(saved)=Qw*[ fun(QxR) .* (i-QxR)./abs(i-QxR).^3 ];
+%     ExtL(saved)=Qw*[ fun(QxL) .* (i-QxL)./abs(i-QxL).^3 ];
+%     saved=saved+1;
+% end
+% plot(Centroids(resolve),Decomp+ExtR+ExtL,':b')
+
+% %Global quadrature with singularity omission
+% h = waitbar(0,'Initializing GL points please wait...');
+% N=500;
+% [Qx,Qw]=GLquad(N);
+% Qx = Qx*(B-A)/2+(B+A)/2;
+% saved=1;
+% resolve = 1:round(N/numel(Decomp)):N; %Evaluate at same frequency as Decomp for speed comparison purposes
+% for i=resolve
+%     waitbar(saved/numel(resolve),h,'Global Omit method...')
+%     NOi = [1:i-1,i+1:N];
+%     GlobOmit(saved) = Qw(NOi)*[ fun(Qx(NOi)) .* (Qx(i)-Qx(NOi))./abs(Qx(i)-Qx(NOi)).^3 ];
+%     saved=saved+1;
+% end
+% GlobOmit = GlobOmit*(B-A)/2;
+% plot(Qx(resolve),GlobOmit,'r')
 
 % %Split Quadrature
 % SP = Qx(resolve);
@@ -94,4 +108,4 @@ plot(Qx(resolve),GlobOmit,'r')
 % end
 % plot(SP,NFSplit(1,:)+NFSplit(2,:)+NFSplit(3,:)+NFSplit(4,:),'c')
 
-close(h)
+%close(h)
