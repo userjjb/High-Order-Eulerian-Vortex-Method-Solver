e45d968c2b3876136567a28a38b6f8aa5ff24e21
Josh Bevan <userjjb@gmail.com>
2015-03-30 19:26:16 -0400

    Effective centroid experimentation in OnWh

--- Utils/Experiments/Biot Savart/OneWheel.m
+++ Utils/Experiments/Biot Savart/OneWheel.m
@@ -1,9 +1,11 @@
-a=0.1;
-b=0.1;
-g=@(x,y) 15*exp(-((x-.5).^2/a+(y-.5).^2/b));
+a=0.3;
+b=0.7;
+dx=.1; 
+dy=.2;
+g=@(x,y) 15*exp(-((x-dx).^2/a+(y-dy).^2/b));%Center is at (dx,dy)
 r=@(x,y,i,j) 1./((i+x).^2+(j+y).^2);
 
-N=10;
+N=8;
 [Qx,Qw] = GLquad(N);
 
 nd=(Qx+1)/2;
@@ -17,18 +19,32 @@ end
 interp_g=@(x,y) reshape(diag(Lag(reshape(x,1,[]),1:N)'*(G*Lag(reshape(y,1,[]),1:N)))',size(x));
 
 q=integral2(g,0,1,0,1);
-for i=1:20
+qq=integral2(interp_g,0,1,0,1);
+for i=0:20
+    ii=i+1;
     for j=1:20
         duo=@(x,y) g(x,y).*r(x,y,i,j);
         iduo=@(x,y) interp_g(x,y).*r(x,y,i,j);
-        E(i,j)=integral2(duo,0,1,0,1);
-        I(i,j)=integral2(iduo,0,1,0,1);
+        E(ii,j)=integral2(duo,0,1,0,1);
+        I(ii,j)=integral2(iduo,0,1,0,1);
         for k=1:N
             for m=1:N
                 R(k,m)=r(nd(k),nd(m),i,j);
             end
         end
-        Q(i,j)=Qw/4*(G.*R)*Qw';
-        OW(i,j)=q/((i+.5)^2+(j+.5)^2);
+        Q(ii,j)=Qw/4*(G.*R)*Qw';
+        OW(ii,j)=qq/((i+.332)^2+(j+.429)^2);
     end
 end
+
+difEOW=E-OW;
+difEQ=E-Q;
+difQOW=Q-OW;
+rrr=q./E;
+rrrs=sqrt(rrr);
+[Y2, X2]= meshgrid(1:20,0:20);
+rr=(X2+.332).^2+(Y2+.429).^2;
+rrd=rr-rrr;
+[X, Y]= meshgrid(0:.01:1,0:.01:1);
+%surf(X,Y,g(X,Y))
+norm(rrd)
--- Utils/Experiments/Biot Savart/RMconverge.m
+++ Utils/Experiments/Biot Savart/RMconverge.m
@@ -1,11 +1,9 @@
-clear all
-
 K=@(x,d) x./(x.^2 +d^2).^(3/2);
 KK=@(x) 1./x.^2;
     c2 = 5/100;
-w=@(x) exp(-(x+.1).^2/(2*c2));
+w=@(x) exp(-(x+0).^2/(2*c2));
 
-duo=@(x) K(x+1,.1).*w(x);
+duo=@(x) K(x+5,.01).*w(x);
 q=integral(duo,-1,1,'RelTol',1e-14,'AbsTol',1e-16)
 
 N=9;
@@ -16,7 +14,13 @@ Lag3= @(x,nv) prod(bsxfun(@rdivide,bsxfun(@minus,x,pp(nv,:,:)),bsxfun(@minus,Qx(
 g=duo(Qx')*Qw'
 interp=@(x) duo(Qx')*Lag3(x,1:N);
 
-xx=-1:.01:1;
+xx=-1:.0001:1;
 plot(xx,duo(xx),'k')
 hold on
 plot(xx,interp(xx),'r')
+
+dd=duo(xx);
+tot=sum(duo(xx));
+for j=1:length(xx)
+    s(j)=sum(dd(1:j));
+end
