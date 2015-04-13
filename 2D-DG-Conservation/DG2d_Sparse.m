95db2bcc103321e6c042db37b0610146b8a9adbf
Josh Bevan <userjjb@gmail.com>
2015-04-13 12:29:25 -0400

    LSERK time marching

--- 2D-DG-Conservation/DG2d_Sparse.m
+++ 2D-DG-Conservation/DG2d_Sparse.m
@@ -10,15 +10,19 @@ clear all
 clc
 %Solver parameters
 alpha= 1;                           %Numerical flux param (1 upwind,0 CD)
-N= 4;                               %Local vorticity poly order
+N= 8;                               %Local vorticity poly order
 M= N;                               %Local velocity poly order
+[RKa,RKb,RKc,nS]= LSRKcoeffs('NRK14C');
 %Global domain initialization (parameters)---------------------------------
 B= [-1 1 -1 1];                     %left, right, bottom, top
-K= [10 10];                         %Num elements along x,y
+K= [32 32];                         %Num elements along x,y
 Ex= linspace(B(1),B(2),K(1)+1);     %Elem edges left-right
 Ey= linspace(B(3),B(4),K(2)+1);     %Elem edges bottom-top
-cx= sqrt(2)/2;
-cy= sqrt(2)/2;
+
+c=1;
+theta = pi/4;
+cx = c*cos(theta);
+cy = c*sin(theta);
 %Global domain initialization (calculated)---------------------------------
 Np= N+1;                            %Number of interpolation points to achieve N
 Mp= M+1;                            %Number of interpolation points to achieve M
@@ -72,18 +76,27 @@ h_y=(diff([B(3)+(B(3)-y_w(1));y_w]) + diff([y_w; B(4)+(B(4)-y_w(end))]))/2;
 [h_x, h_y]= meshgrid(h_x,h_y);
 norm_h=h_x.*h_y;
 
+%2D mollifier with center dx,dy and range over the ellipse with axes a,b
+moll=@(x,y,dx,dy,a,b) heaviside(1-(((x-dx)/a).^2+((y-dy)/b).^2)).*exp(1+(-1./(1-(((x-dx)/a).^2+((y-dy)/b).^2).^4)));
 %Intial conditions specification-------------------------------------------
 ICfuns={}; %Create a cell list of functions that define the ICs
     %Gaussian 1
-Ga1=    0.1;
-Gb1=    0.1;
+Ga1=    0.05;
+Gb1=    0.05;
 Gdx1=   0; 
 Gdy1=   0;
 GA1=    1;
-ICfuns{end+1}=@(x,y,t) GA1*exp(-(((x)-Gdx1).^2/Ga1+(y-Gdy1).^2/Gb1));%Center is at (dx,dy)
+ICfuns{end+1}=@(x,y) GA1*exp(-(((x)-Gdx1).^2/Ga1+(y-Gdy1).^2/Gb1));%Center is at (dx,dy)
+    %Gaussian 2
+Ga2=    0.02;
+Gb2=    0.05;
+Gdx2=   0.5; 
+Gdy2=   -0.7;
+GA2=    .4;
+ICfuns{end+1}=@(x,y) GA2*exp(-(((x)-Gdx2).^2/Ga2+(y-Gdy2).^2/Gb2));%Center is at (dx,dy)
 
 %Iterate over each of the IC funs
-for IC=1:numel(ICfuns)
+for IC=1:1%numel(ICfuns)
     w=w+ICfuns{IC}(wxm,wym);
 end
 %clearvars wxm wym %No longer need the meshgrid points
@@ -102,35 +115,41 @@ wy=   reshape(w,Np,1,[]);       %Reshape vorticity for mtimesx_y bsx
 v_x=  reshape(v_x',1,Mp,[]);    %Reshape velocity_x for mtimesx bsx
 v_y=  reshape(v_y,1,Mp,[]);     %Reshape velocity_x for mtimesx bsx
 
-delt=0.0005;
-skip=0.05;
-for t=0:delt:3
-    w_lx= mtimesx(Ll',wx);          %Left interpolated vorticity
-    w_rx= mtimesx(Lr',wx);          %Right interpolated vorticity
-    w_bx= mtimesx(Ll',wy);          %Bottom interpolated vorticity
-    w_tx= mtimesx(Lr',wy);          %Top interpolated vorticity
-    %Boundary fluxes
-    fl= abs( v_x(EBl) ).*( w_rx(x_km1).*(sign(v_x(EBl))+alpha) + w_lx.*(sign(v_x(EBl))-alpha) );
-    fr= abs( v_x(EBr) ).*( w_rx.*(sign(v_x(EBr))+alpha) + w_lx(x_kp1).*(sign(v_x(EBr))-alpha) );
-    fb= abs( v_y(EBb) ).*( w_tx(y_km1).*(sign(v_y(EBb))+alpha) + w_bx.*(sign(v_y(EBb))-alpha) );
-    ft= abs( v_y(EBt) ).*( w_tx.*(sign(v_y(EBt))+alpha) + w_bx(y_kp1).*(sign(v_y(EBt))-alpha) );
-    
-    SurfFlux_x=bsxfun(@times,fr,LrM)-bsxfun(@times,fl,LlM); %Nodal total surface flux
-    Stiff_x= mtimesx(v_x,mtimesx(QwSM,wx)); %Nodal stiffness eval
-    SurfFlux_y=bsxfun(@times,ft,LrM)-bsxfun(@times,fb,LlM);
-    Stiff_y= mtimesx(v_y,mtimesx(QwSM,wy));
+delt= 0.005;
+skip= 0.5;
+k2=   zeros(size(wx));
+for t=0:delt:100
+    for i=1:nS
+        St= t+RKc(i)*delt;              %Unused currently, St is the stage time if needed
+        w_lx= mtimesx(Ll',wx);          %Left interpolated vorticity
+        w_rx= mtimesx(Lr',wx);          %Right interpolated vorticity
+        w_bx= mtimesx(Ll',wy);          %Bottom interpolated vorticity
+        w_tx= mtimesx(Lr',wy);          %Top interpolated vorticity
+        %Boundary fluxes
+        fl= abs( v_x(EBl) ).*( w_rx(x_km1).*(sign(v_x(EBl))+alpha) + w_lx.*(sign(v_x(EBl))-alpha) );
+        fr= abs( v_x(EBr) ).*( w_rx.*(sign(v_x(EBr))+alpha) + w_lx(x_kp1).*(sign(v_x(EBr))-alpha) );
+        fb= abs( v_y(EBb) ).*( w_tx(y_km1).*(sign(v_y(EBb))+alpha) + w_bx.*(sign(v_y(EBb))-alpha) );
+        ft= abs( v_y(EBt) ).*( w_tx.*(sign(v_y(EBt))+alpha) + w_bx(y_kp1).*(sign(v_y(EBt))-alpha) );
 
-    wx_dt= permute(Stiff_x-SurfFlux_x,[4 1 3 2]); %Reshape to match wx
-    wy_dt= reshape(reshape(Stiff_y-SurfFlux_y,K(2),[])',Np,1,[]); %Reshape to match wx
-    wx= wx+(wx_dt+wy_dt)*delt;
-    wy= reshape(reshape(wx,K(1)*Np,[])',Np,1,[]); %Reshape wx to match global node ordering
+        SurfFlux_x=bsxfun(@times,fr,LrM)-bsxfun(@times,fl,LlM); %Nodal total surface flux
+        Stiff_x= mtimesx(v_x,mtimesx(QwSM,wx)); %Nodal stiffness eval
+        SurfFlux_y=bsxfun(@times,ft,LrM)-bsxfun(@times,fb,LlM);
+        Stiff_y= mtimesx(v_y,mtimesx(QwSM,wy));
+
+        wx_dt= permute(Stiff_x-SurfFlux_x,[4 1 3 2]); %Reshape to match wx
+        wy_dt= reshape(reshape(Stiff_y-SurfFlux_y,K(2),[])',Np,1,[]); %Reshape to match wx
+        
+        k2= RKa(i)*k2 + delt*(wx_dt+wy_dt);
+        wx= wx+RKb(i)*k2;
+        wy= reshape(reshape(wx,K(1)*Np,[])',Np,1,[]); %Reshape wx to match global node ordering
+    end
     if mod(t,skip)<delt
         surf(wxm,wym,reshape(wx,Np*K(1),Np*K(2))')
-        axis([B,0,1])
-        text(-1,1,1.1,num2str(t));
+        axis([B,0,1.1])
+        text(-1,1,1.2,strcat('Time: ',num2str(t)));
         %Residual calc, used to calc the L^2 norm
         R=GA1*exp(-((mod((wxm+1)/2-t*cx/2,1)*2-1).^2/Ga1+(mod((wym+1)/2-t*cy/2,1)*2-1).^2/Gb1))-reshape(wx,Np*K(1),Np*K(2))';
-        text(-1,1,1.2,num2str(sqrt(sum(sum(norm_h.*R.^2)))));
-        pause(0.001)
+        text(-1,1,1.3,strcat('L^2 norm: ',num2str(sqrt(sum(sum(norm_h.*R.^2))))));
+        pause(0.0001)
     end
 end
