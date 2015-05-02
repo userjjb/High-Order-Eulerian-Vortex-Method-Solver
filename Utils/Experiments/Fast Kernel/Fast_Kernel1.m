74fc9f4331f93097d1541118efb69b4f6b4da0c3
Josh Bevan <userjjb@gmail.com>
2015-05-01 23:53:50 -0400

    Fast Kernel

---
+++ Utils/Experiments/Fast Kernel/Fast_Kernel1.m
@@ -0,0 +1,21 @@
+clear all
+N=3;
+Np=1+N;
+delX=0.2;
+Ex=-1:delX:1;
+
+[Qx,Qw]=LGLquad(Np);
+nd=(Qx+1)/(2/delX);
+
+temp=bsxfun(@plus,nd,Ex(1:end-1));
+xv=[reshape(temp(1:end-1,:),[],1);Ex(end)];
+
+[xM,yM]=meshgrid(xv,xv);
+
+del=0.1;
+Ky=@(x,y) (xM-x)./((xM-x).^2+(yM-y).^2+del^2).^(3/2);
+
+Ky11=Ky(0,0);
+Ky12=Ky(nd(2),0);
+Ky21=Ky(0,nd(2));
+Ky13=Ky(nd(3),0);
